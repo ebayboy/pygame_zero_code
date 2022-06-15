68595c67311f3c9db6dc0166d29bdde38540542d
fanpengfei <fanpengfei1@jd.com>
2022-06-15 14:11:04 +0800

    fw

--- src/simple_gui/simple_gui.py
+++ src/simple_gui/simple_gui.py
@@ -1,7 +1,7 @@
 import PySimpleGUI as sg
 
 layout = [
-    [sg.Output(size=(10, 1), key='number', pad=(5,5))],
+    [sg.Output(size=(10, 1), key='number', pad=(5, 5))],
     [sg.Button('7'), sg.Button('8'), sg.Button('9')],
     [sg.Button('4'), sg.Button('5'), sg.Button('6')],
     [sg.Button('1'), sg.Button('2'), sg.Button('3')]
